5cf2aaf69eeadb8b867269e08a7e7a28cb17aa57
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-21 09:37:54 +0530

    classes and object

---
+++ ToDoJavaProject/src/org/SecondExample.java
@@ -0,0 +1,28 @@
+package org;
+
+public class SecondExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// >,<,>=,<=,==,!=
+		// it returns true or false
+		int a,b;
+		a=500;
+		b=400;
+		boolean res;
+		res=a>b;
+		System.out.println("Result  is ->" +res);
+		res=a<b;
+		System.out.println("Result  is ->" +res);
+		res=a>=b;
+		System.out.println("Result  is ->" +res);
+		res=a<=b;
+		System.out.println("Result  is ->" +res);
+		res=a==b;
+		System.out.println("Result  is ->" +res);
+		res=a!=b;
+		System.out.println("Result  is ->" +res);
+		
+	}
+
+}

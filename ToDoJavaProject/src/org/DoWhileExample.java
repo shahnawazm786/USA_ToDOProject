7888edc6425eac6693b98924e6837d8e00aab038
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-23 09:17:23 +0530

    23 feb

--- ToDoJavaProject/src/org/DoWhileExample.java
+++ ToDoJavaProject/src/org/DoWhileExample.java
@@ -4,6 +4,10 @@ public class DoWhileExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// while if condition is wrong - it wont execute
+		// do -while even condition is wrong - it will execute at least one times
+		
+		
 		
 	}
 

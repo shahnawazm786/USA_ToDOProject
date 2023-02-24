eccd54927561b0763fab2d1700673093ce620b1d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-24 09:23:45 +0530

    feb

---
+++ ToDoJavaProject/src/org/ContinueKeyWordExample.java
@@ -0,0 +1,14 @@
+package org;
+
+public class ContinueKeyWordExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			if(i<5)
+				continue;
+			System.out.println(i);
+		}
+	}
+
+}

fa2dc43ace0493a94e89322efdf0f1139a1c03a9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-24 08:37:50 +0530

    feb 23

---
+++ ToDoJavaProject/src/org/ForLoopExample.java
@@ -0,0 +1,21 @@
+package org;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Step 1 - initialization 
+		// Step 2 - condition check
+		// step 3 - increment/decrement
+		int i;
+		for(i=0; i<=10; i++) {
+			System.out.println("Hello!! Java");
+		}
+		int j=0;
+		for(;j<=10;) {
+			System.out.println("Hi!!!!");
+			j++;
+		}
+	}
+
+}

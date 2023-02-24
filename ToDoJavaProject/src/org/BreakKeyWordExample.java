d365b93a34b258d05412a502ccb3b3cbae830bde
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-24 09:22:08 +0530

    feb 23

---
+++ ToDoJavaProject/src/org/BreakKeyWordExample.java
@@ -0,0 +1,14 @@
+package org;
+
+public class BreakKeyWordExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;;i++) {
+			if(i>50)
+				break;
+			System.out.print(i+" ");
+		}
+	}
+
+}

b788093fc19d81587c016b321a31d50fc49cae9e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-24 08:53:00 +0530

    feb 23

---
+++ ToDoJavaProject/src/org/ForLoopforObjectExample.java
@@ -0,0 +1,20 @@
+package org;
+
+public class ForLoopforObjectExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="This is java program";
+		for(int i=0; i<str.length();i++) {
+			System.out.print(str.charAt(i)+" ");
+		}
+		String[] word=str.split(" ");
+		System.out.println("\n Count of word ->"+word.length);
+		for(String s : word) {
+			if(s.equals("java")) {
+				System.out.println(s);
+			}
+		}
+	}
+
+}

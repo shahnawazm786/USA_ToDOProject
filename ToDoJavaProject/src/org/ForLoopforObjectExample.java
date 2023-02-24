bc1e2953a5860d4794a792c979cc031023dc41a7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-24 08:58:38 +0530

    23 feb

--- ToDoJavaProject/src/org/ForLoopforObjectExample.java
+++ ToDoJavaProject/src/org/ForLoopforObjectExample.java
@@ -9,16 +9,16 @@ public class ForLoopforObjectExample {
 			System.out.print(str.charAt(i)+" ");
 		}
 		String[] word=str.split(" ");
+		String s="This and That|is|a|java|programing|class";
+		String[] words=s.split("|");
+		System.out.println(words);
 		System.out.println("\n Count of word ->"+word.length);
-		for(String s : word) {
+		for(String s1 : word) {
 			if(s.equals("java")) {
-				System.out.println(s);
+				System.out.println(s1);
 			}
 		}
-		// find vowel from String
-		for(char c : str.toCharArray()) {
-			System.out.println(c);
-		}
+		
 	}
 
 }

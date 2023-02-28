af8f12e887648909b8087e075c8fca75364e2a70
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-28 08:56:34 +0530

    feb

---
+++ ToDoJavaProject/src/org/abstraction/VariableModifierExample.java
@@ -0,0 +1,27 @@
+package org.abstraction;
+
+public class VariableModifierExample {
+// Modifier - private, public, protected, package/default
+// used variable and method
+// abstraction/ data hiding
+	// public
+	// Same class - Yes
+	// Sub/child class - Yes
+	// Same Package in different class - Yes
+	// Different Package  - Yes
+	// Private
+	// Same class - Yes
+	// Sub/child class - No
+	// Same Package in different class - No
+	// Different Package  - No
+	// Protected
+	// Same class - Yes
+	// Sub/child class - Yes
+	// Same Package in different class - Yes
+	// Different Package  - No
+	// Default / Pacakge
+	// Same class - Yes
+	// Sub/child class - Yes
+	// Same Package in different class - No
+	// Different Package  - No
+}

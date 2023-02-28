2bbce4f972b735ef3aa8b0e45770597bfdd14017
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-28 08:43:55 +0530

    28 feb

--- ToDoJavaProject/src/org/StartPatternExample.java
+++ ToDoJavaProject/src/org/StartPatternExample.java
@@ -4,7 +4,12 @@ public class StartPatternExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }

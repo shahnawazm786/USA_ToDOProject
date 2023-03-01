5106e89e11cd84f6eeea703005ac9c4c0d8b51f2
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-01 08:52:38 +0530

    1 mar

--- ToDoJavaProject/src/abstractandinterface/InterfaceDemo.java
+++ ToDoJavaProject/src/abstractandinterface/InterfaceDemo.java
@@ -5,6 +5,10 @@ public class InterfaceDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculus cal=new Calculus("Lalji Parsad","Integral Calculus","Lal ji");
+		System.out.println(cal.publisherName());
+		System.out.println(cal.authorName());
+		System.out.println(cal.writeName());
+		System.out.println(cal.bookName());
 	}
 
 }

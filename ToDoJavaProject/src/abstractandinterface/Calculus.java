a373e82a4f467cdf027635fc7cf17138ad2a28bc
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-01 08:49:15 +0530

    1 mar

--- ToDoJavaProject/src/abstractandinterface/Calculus.java
+++ ToDoJavaProject/src/abstractandinterface/Calculus.java
@@ -2,22 +2,33 @@ package abstractandinterface;
 
 public class Calculus implements IBook{
 
+	String Wname;
+	String bName;
+	String aName;
+	public Calculus(String writerName,String bookName,String authorName) {
+		this.Wname=writerName;
+		this.bName=bookName;
+		this.aName=authorName;
+	}
 	@Override
 	public String writeName() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.Wname;
 	}
 
 	@Override
 	public String bookName() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.bName;
 	}
 
 	@Override
 	public String authorName() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.aName;
+	}
+	public String publisherName() {
+		return publisherName;
 	}
 
 }

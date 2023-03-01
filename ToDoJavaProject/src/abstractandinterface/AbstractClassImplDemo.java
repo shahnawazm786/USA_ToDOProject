93e566b3a7382c900ee5a6c71ac84a0358424b2f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-01 09:05:12 +0530

    mar

--- ToDoJavaProject/src/abstractandinterface/AbstractClassImplDemo.java
+++ ToDoJavaProject/src/abstractandinterface/AbstractClassImplDemo.java
@@ -4,7 +4,9 @@ public class AbstractClassImplDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ABook book=new OracleBook();
+		System.out.println(book.bookName());
+		System.out.println(book.writeName());
 	}
 
 }

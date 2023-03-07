d72bbbbc161db15f3bf8e70a9be6dda13d13c33c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-07 08:49:22 +0530

    07 March

---
+++ ToDoJavaProject/src/polymorphism/CalcualteClass.java
@@ -0,0 +1,10 @@
+package polymorphism;
+
+public class CalcualteClass extends AddMethod {
+
+	
+	void add(int a,float b) {
+	System.out.println("add() in calcualte class");
+	System.out.println("Addition of ->"+a +"and ->"+b+"\t="+(a+b));
+}
+}

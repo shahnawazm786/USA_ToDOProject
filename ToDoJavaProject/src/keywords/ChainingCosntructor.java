6572ca9664057181b4ece106d0e8ed1b8c492746
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-08 22:11:23 +0530

    haining

--- ToDoJavaProject/src/keywords/ChainingCosntructor.java
+++ ToDoJavaProject/src/keywords/ChainingCosntructor.java
@@ -1,14 +1,22 @@
 package keywords;
 
 public class ChainingCosntructor {
-	String name;
+	
 	public ChainingCosntructor() {
-		System.out.println(this.name);
+		System.out.println("Constructor without parameter");
 	}
 	public ChainingCosntructor(String nm) {
-		super();
-		this.name=nm;
-		System.out.println(this.name);
+		this();
+		System.out.println("Constructor with one parameter"+nm);
+		//
+		
 	}
+	public ChainingCosntructor(String n,int age) {
+		this(n);
+		System.out.println("Constructor with two parameter"+n +" age is"+age);
+		//
+		
+	}
+	
 
 }

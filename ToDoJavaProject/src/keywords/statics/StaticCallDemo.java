7f898512501ebef3e6dac3ca14615d36821aa125
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-08 21:58:05 +0530

    marhc

---
+++ ToDoJavaProject/src/keywords/statics/StaticCallDemo.java
@@ -0,0 +1,11 @@
+package keywords.statics;
+
+public class StaticCallDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticExample.name="Java";
+		StaticExample.showName();
+	}
+
+}

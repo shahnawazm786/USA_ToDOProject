a42667bf1a1049593f17eec731babe276e18210b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-08 22:11:34 +0530

    haining

---
+++ ToDoJavaProject/src/keywords/Demo.java
@@ -0,0 +1,10 @@
+package keywords;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChainingCosntructor cc=new ChainingCosntructor("Java",20);
+	}
+
+}

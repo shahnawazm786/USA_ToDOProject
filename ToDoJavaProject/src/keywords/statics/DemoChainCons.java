cca0548bce61090dd498abc51765e33f76532b1d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-09 08:57:20 +0530

    march

---
+++ ToDoJavaProject/src/keywords/statics/DemoChainCons.java
@@ -0,0 +1,10 @@
+package keywords.statics;
+
+public class DemoChainCons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ParentClass pc=new ParentClass("Java");
+	}
+
+}

ee37b1aee8b9c382c39ee3f4c98f13fa8ac337e5
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-09 08:57:26 +0530

    marc

--- ToDoJavaProject/src/keywords/statics/ParentClass.java
+++ ToDoJavaProject/src/keywords/statics/ParentClass.java
@@ -1,5 +1,11 @@
 package keywords.statics;
 
 public class ParentClass {
-
+	
+	public ParentClass() {
+		System.out.println("Default Constructor");
+	}
+	public ParentClass(String para) {
+		System.out.println("Constructor with one paramenter ->"+para);
+	}
 }

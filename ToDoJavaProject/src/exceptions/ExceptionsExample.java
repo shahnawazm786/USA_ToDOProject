f3514beb3a670ba6ab1ba6f6091264ea4982004c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-09 09:15:23 +0530

    march

---
+++ ToDoJavaProject/src/exceptions/ExceptionsExample.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class ExceptionsExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		a=100;b=200;
+		System.out.println(a+b);
+	}
+
+}

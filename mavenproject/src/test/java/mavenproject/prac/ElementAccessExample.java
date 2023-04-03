44fef500a0ca9f66d157aaedbb4d446169c671ae
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-03 09:59:54 +0530

    a

--- mavenproject/src/test/java/mavenproject/prac/ElementAccessExample.java
+++ mavenproject/src/test/java/mavenproject/prac/ElementAccessExample.java
@@ -1,15 +1,17 @@
 package mavenproject.prac;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ElementAccessExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();// inheritance method overriding
@@ -21,7 +23,9 @@ public class ElementAccessExample {
 		//inpuByCssSelector(driver);
 		//inpuByClass(driver);
 		//anchorByPartialText(driver);
-		anchorByLinkText(driver);
+		//anchorByLinkText(driver);
+		//textareaInput(driver);
+		tagName(driver);
 
 	}
 	public static void inputByName(WebDriver driver) {
@@ -55,6 +59,31 @@ public class ElementAccessExample {
 	public static void anchorByLinkText(WebDriver driver) {
 		WebElement anchor = driver.findElement(By.partialLinkText("Return to index"));
 		anchor.click();
+	}
+	public static void textareaInput(WebDriver driver) {
+		WebElement textarea = driver.findElement(By.name("my-textarea"));
+		textarea.sendKeys("Hello");
+		textarea.sendKeys(Keys.ENTER);
+		textarea.sendKeys("Hello 1");
+		textarea.sendKeys(Keys.ENTER);
+		textarea.sendKeys("Hello 2");
+		textarea.sendKeys(Keys.ENTER);
+			
+	}
+	public static void tagName(WebDriver driver) throws Exception {
+	List<WebElement> input = driver.findElements(By.tagName("input"));
+	for(WebElement ele:input) {
+		System.out.println(ele.getAttribute("type"));
+	}
+	if(input.size()==14) {
+		System.out.println("14 element founds");
+		//throw new Exception("14 elements found");
+	}
+	else
+	{
+		System.out.println("14 element not founds");
+		//throw new Exception("Size mis-matched");
+	}
 	}
 	
 }

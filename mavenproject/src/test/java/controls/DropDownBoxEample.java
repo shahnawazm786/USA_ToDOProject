cfa5dc11b77a931b61733a7e3fec1a5a419a3969
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-04 09:20:40 +0530

    drop

--- mavenproject/src/test/java/controls/DropDownBoxEample.java
+++ mavenproject/src/test/java/controls/DropDownBoxEample.java
@@ -1,10 +1,40 @@
 package controls;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class DropDownBoxEample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+	private static WebDriver driver;
 
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		driver=CommonMethods.getDriver();
+		//selectDropDownBoxByValue(driver);
+		Thread.sleep(2000);
+		selectDropDownBoxByVisibleText(driver);
+		driver.quit();
+	}
+	public static void selectDropDownBoxByValue(WebDriver driver) throws Exception {
+		WebElement ele=CommonMethods.findElementByLocator(driver, "name", "my-select");
+		Select sel=new Select(ele);
+		sel.selectByValue("1");
+		Thread.sleep(5000);
+		sel.selectByValue("2");
+		Thread.sleep(5000);
+		sel.selectByValue("3");
+		Thread.sleep(5000);
+	}
+	public static void selectDropDownBoxByVisibleText(WebDriver driver) throws Exception {
+		WebElement ele=CommonMethods.findElementByLocator(driver, "name", "my-select");
+		Select sel=new Select(ele);
+		sel.selectByVisibleText("One");
+		Thread.sleep(5000);
+		sel.selectByVisibleText("Two");
+		Thread.sleep(5000);
+		sel.selectByVisibleText("Three");
+		Thread.sleep(5000);
+	}
+	
 }
---
+++ mavenproject/src/test/java/controls/DropDownWithInputTextBox.java
@@ -0,0 +1,10 @@
+package controls;
+
+public class DropDownWithInputTextBox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

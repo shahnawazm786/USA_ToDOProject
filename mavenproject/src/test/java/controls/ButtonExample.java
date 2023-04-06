9c8d7edb54bf18382fd42065a63ed38af60d0ab2
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-06 08:53:45 +0530

    a

--- mavenproject/src/test/java/controls/ButtonExample.java
+++ mavenproject/src/test/java/controls/ButtonExample.java
@@ -1,9 +1,15 @@
 package controls;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class ButtonExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver driver = CommonMethods.getDriver();
+		WebElement ele = CommonMethods.findElementByLocator(driver, "tagname", "button");
+		ele.click();
 
 	}
 

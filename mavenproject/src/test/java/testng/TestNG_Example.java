496e2a41e995eb4e972fecb4dcfb27b7e95ae091
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-05-04 12:38:37 +0530

    a

--- mavenproject/src/test/java/testng/TestNG_Example.java
+++ mavenproject/src/test/java/testng/TestNG_Example.java
@@ -1,5 +1,37 @@
 package testng;
 
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
 public class TestNG_Example {
 
+	@Test
+	public void my_login() {
+		System.out.println("Login");
+	}
+	
+	@Test
+	public void my_log() {
+		System.out.println("Login");
+	}
+	
+	
+	@Test(priority = 0)
+	public void test() {
+		System.out.println("Hello Program");
+	}
+	
+	@Test(priority = 1)
+	public void test1() {
+		System.out.println("Failed");
+		Assert.assertFalse(true);
+	}
+	
+	
+	@Test(priority = 2)
+	public void test3() {
+		throw new SkipException("Skip");
+	}
+	
 }

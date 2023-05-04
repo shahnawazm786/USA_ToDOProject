c423549631e6ffb4e1b133dd829347ab63185f16
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-05-04 12:50:14 +0530

    web

--- mavenproject/src/test/java/testng/OnLineRetailsApps.java
+++ mavenproject/src/test/java/testng/OnLineRetailsApps.java
@@ -1,12 +1,21 @@
 package testng;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OnLineRetailsApps {
 	WebDriver driver;
 	@BeforeSuite
 	public void setup() {
-		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().window().maximize();
+
 	}
 }

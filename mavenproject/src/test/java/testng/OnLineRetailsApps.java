f4480711e41922a09cefdc882d627b58f568a95c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-05-04 12:46:32 +0530

    web driver manager

--- mavenproject/src/test/java/testng/OnLineRetailsApps.java
+++ mavenproject/src/test/java/testng/OnLineRetailsApps.java
@@ -1,10 +1,12 @@
 package testng;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeSuite;
 
 public class OnLineRetailsApps {
+	WebDriver driver;
 	@BeforeSuite
 	public void setup() {
-
+		
 	}
 }

4338eedca0aba5a92b69ce3807a470e8a882dd1a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-05-07 21:45:17 +0530

    DataProviderExample.class

--- mavenproject/src/test/java/testng/DataProiver_1.java
+++ mavenproject/src/test/java/testng/DataProiver_1.java
@@ -6,5 +6,10 @@ public class DataProiver_1 {
 	public void showValue(String val) {
 		System.out.println(val);
 	}
+	
+	@Test(dataProvider = "multi-data-provider",dataProviderClass = DataProviderExample.class)
+	public void multipParaShowValue(int x,int y,int z) {
+		System.out.println("Sum is \t"+(x+y+z));
+	}
 
 }

32e8e6d878d1505968bd85101ed17de76dedcf9b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-05-11 09:20:59 +0530

    https://www.nopcommerce.com/en/demo

--- mavenproject/src/test/java/stepdefs/Runner/CucumberRunnerClass.java
+++ mavenproject/src/test/java/stepdefs/Runner/CucumberRunnerClass.java
@@ -5,7 +5,12 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		features = "src/test/resources/features",
-		glue = ""
+		glue = "stepdefs",
+		tags="@login_valid_credential",
+		plugin= {"pretty","html:target/example/Report.html",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"	
+		}
+		
 		)
 public class CucumberRunnerClass extends AbstractTestNGCucumberTests {
 

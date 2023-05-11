8c2351febbedf57fb435c512c148d741297fffa4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-05-11 09:21:02 +0530

    https://www.nopcommerce.com/en/demo

--- mavenproject/src/test/java/stepdefs/Runner/CucumberRunnerTests.java
+++ mavenproject/src/test/java/stepdefs/Runner/CucumberRunnerTests.java
@@ -7,11 +7,12 @@ import io.cucumber.testng.CucumberOptions;
 		features = "src/test/resources/features",
 		glue = "stepdefs",
 		tags="@cart_shoes",
-		dryRun = true,
-		monochrome = true,
-		plugin = {"pretty","json:target/cucumber-reports/cucumber.json",
-				"html:target/cucumber-reports/cucumberreport.html",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+				dryRun = true,
+				monochrome = true,
+				plugin = {"pretty","json:target/cucumber-reports/cucumber.json",
+						"html:target/cucumber-reports/cucumberreport.html",
+						"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+		
 		 
 		
 		)

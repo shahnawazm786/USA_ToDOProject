582e90b18e8fcbae1fd67a914e6d8a0e3f13535e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-06-04 15:59:48 +0530

    abc

--- mavenproject/src/test/java/com/example/runner/Runner.java
+++ mavenproject/src/test/java/com/example/runner/Runner.java
@@ -1,5 +1,16 @@
 package com.example.runner;
 
-public class Runner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		features = "src/test/resources/features",
+		glue = "com.example.stepdefs",
+		tags="@nopsanity",
+		plugin= {"pretty","html:target/example/Report.html",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"	
+		}
+		)
+public class Runner extends AbstractTestNGCucumberTests {
 
 }

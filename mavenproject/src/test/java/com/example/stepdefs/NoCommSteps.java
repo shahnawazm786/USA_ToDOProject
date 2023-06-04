c93cb781fbb5d79dd7ea15a69e0de586828528cf
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-06-04 15:59:13 +0530

    abc

--- mavenproject/src/test/java/com/example/stepdefs/NoCommSteps.java
+++ mavenproject/src/test/java/com/example/stepdefs/NoCommSteps.java
@@ -1,5 +1,45 @@
 package com.example.stepdefs;
 
-public class NoCommSteps {
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.HeaderPage;
+import pages.HomePage;
+
+public class NoCommSteps{
+	
+	HomePage homePage=new HomePage(Hooks.driver);
+	HeaderPage headerPage=new HeaderPage(Hooks.driver);
+	@Given("Validate home page title")
+	public void open_the_application() {
+	   homePage.validateTitle("Free and open-source eCommerce platform. ASP.NET Core based shopping cart. - nopCommerce");
+	}
+
+	@When("clik on get started")
+	public void clik_on_get_started() {
+	   homePage.navigateGetStart();
+	}
+
+	@Then("verify the title of the page")
+	public void verify_the_title_of_the_page() {
+	    homePage.validateGetStartPageTitle("Get started with nopCommerce - nopCommerce12345");
+	}
+	
+	
+	
+	
+/*
+ *  Product page steps
+ */
+	@When("clik on product menu")
+	public void clik_on_product_menu() {
+    
+}
+
+	@Then("verify the title of the product page")
+	public void verify_the_title_of_the_product_page() {
+    
+	}
+
 
 }
